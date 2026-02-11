39be14232837e7edf564f2c4a31fc1161bd43837
manaswiniboja <Manaswini.B@proventech.in>
2026-02-11 16:39:06 +0530

    settings.py

--- project2/settings.py
+++ project2/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cwqbk^n)qfj_tjo0aoq17x3mhfqh^^t2nrmlwt4#je3l26so@6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'http://127.0.0.1:8080','your-project.up.railway.app']
 
 
 # Application definition
