06d2219433b191901272d922424d224e5c8cb1ce
manaswiniboja <Manaswini.B@proventech.in>
2026-02-12 11:29:19 +0530

    settings.py

--- project2/settings.py
+++ project2/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default-key')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 # Applications
