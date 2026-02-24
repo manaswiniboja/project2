f8c4d4192f530f70eaf41ed6304556c2e2fda692
manaswiniboja <Manaswini.B@proventech.in>
2026-02-24 15:16:50 +0530

    asdfgh

--- app2/models.py
+++ app2/models.py
@@ -62,18 +62,8 @@ from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 
 class StudentID(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=128)  
-
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-        self.save()
-
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
-
-    def __str__(self):
-        return self.username
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
 
 
 class FacultyID(models.Model):
